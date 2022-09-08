b0f61120afb0ac334b5d1ca6f2f96d5e1ae7aa57
David Richard <davidrichardf.lima@gmail.com>
2022-09-08 13:26:00 -0300

    Feito os exercicios 6 até o 10

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 12.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if ( vel > 60){
+    console.log(`Você ultrapassou o limite de velocidade`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Nascido em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Ola brasileiro')
+}else{
+    console.log('Ola estrangeiro')
+}
