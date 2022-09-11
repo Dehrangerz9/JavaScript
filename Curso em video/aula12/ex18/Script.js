3cf22127488a2bff0764da43b3bd26ffaa54b622
David Richard <davidrichardf.lima@gmail.com>
2022-09-11 20:20:37 -0300

    Update Script.js

--- Curso em video/aula12/ex18/Script.js
+++ Curso em video/aula12/ex18/Script.js
@@ -8,7 +8,7 @@
         tabuada.innerHTML = ''
         for(var c = 1; c <= 10; c++){
             let item = document.createElement('option')
-            item.text = `${n} x ${c} = 0`
+            item.text = `${n} x ${c} = ${n * c}`
             tabuada.appendChild(item)
         }
     }
